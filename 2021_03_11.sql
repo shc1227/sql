f306b0fef1abdb3220d06194da98cf2f9ebdfbcb
DESKTOP-HF133TA\PC-23 <heepang1227@gmail.com>
2021-03-11 12:50:57 +0900

    SELECT  쿼리 기본문법

---
+++ 2021_03_11.sql
@@ -0,0 +1,16 @@
+SELECT *
+FROM emp;
+--데이터 조회 방법
+--  FROM : 데이터를 조회할 테이블 명시
+--  SELECT : 테이블에 있는 컴럼명, 조회 하고자 하는 컴럼명
+             테이블의 모든 컴럼을 조회할 경우 *를 기술
+SELECT *
+FROM EMP;
+
+SELECT *
+FROM EMP;
+
+--EMPNO : 직원번호, ENAME;직원이름, JOB:담당업무, 
+--MGR:상위 담당자 ,HRIEDATE:고용일자 ,SAL:급여 ,
+--COMM:커미션 ,DEPTNO: 부서번호
+
